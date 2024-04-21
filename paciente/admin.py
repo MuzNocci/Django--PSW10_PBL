2916631ed84ad43e19b444957bbd41477480e0cf
Müller Nocciolli <muller.nocciolli@gmail.com>
2024-04-21 15:12:28 -0300

    AULA03 - Created the consultation pages

--- paciente/admin.py
+++ paciente/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from paciente.models import Consulta, Documento
 
-# Register your models here.
+
+
+admin.site.register(Consulta)
+admin.site.register(Documento)
--- paciente/views.py
+++ paciente/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from medico.models import DadosMedico, Especialidades, DatasAbertas
+from medico.models import DadosMedico, Especialidades, DatasAbertas, is_medico
 from paciente.models import Consulta
 from datetime import datetime
 from django.contrib import messages
+from django.db import transaction
 
 
 
@@ -21,7 +22,7 @@ def home(request):
         if especialidades_filtrar:
             medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
 
-        return render(request, 'home.html', {'medicos':medicos, 'especialidades':especialidades})
+        return render(request, 'home.html', {'medicos':medicos, 'especialidades':especialidades, 'is_medico': is_medico(request.user)})
     
 
 def escolher_horario(request, id_dados_medicos):
@@ -31,24 +32,26 @@ def escolher_horario(request, id_dados_medicos):
         medico = DadosMedico.objects.get(id=id_dados_medicos)
         datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now()).filter(agendado=False)
         
-        return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas})
+        return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas, 'is_medico': is_medico(request.user)})
     
 
 def agendar_horario(request, id_data_aberta):
+    
     if request.method == "GET":
-        data_aberta = DatasAbertas.objects.get(id=id_data_aberta)
 
-        horario_agendado = Consulta(
-            paciente=request.user,
-            data_aberta=data_aberta
-        )
+        #TODO: tornar atomico
 
-        horario_agendado.save()
+        with transaction.atomic():
 
-        # TODO: Sugestão Tornar atomico
+            data_aberta = DatasAbertas.objects.get(id=id_data_aberta)
+            horario_agendado = Consulta(
+                paciente=request.user,
+                data_aberta=data_aberta
+            )
+            horario_agendado.save()
 
-        data_aberta.agendado = True
-        data_aberta.save()
+            data_aberta.agendado = True
+            data_aberta.save()
 
         messages.add_message(request, messages.constants.SUCCESS, 'Horário agendado com sucesso.')
 
@@ -58,9 +61,23 @@ def agendar_horario(request, id_data_aberta):
 def minhas_consultas(request):
 
     if request.method == "GET":
+
+        especialidades_filtrar = request.GET.get('especialidade')
+        date_filtrar = request.GET.get('data')
         
         #TODO: desenvolver filtros
 
-        minhas_consultas = Consulta.objects.filter(paciente=request.user).filter(data_aberta__data__gte=datetime.now())
+        minhas_consultas = Consulta.objects.filter(paciente=request.user).filter(data_aberta__data__gte=datetime.now())       
         
-        return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas})
+        return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas, 'is_medico': is_medico(request.user)})
+    
+
+def consulta(request, id_consulta):
+
+    if request.method == 'GET':
+
+        consulta = Consulta.objects.get(id=id_consulta)
+        dado_medico = DadosMedico.objects.get(user=consulta.data_aberta.user)
+        
+        return render(request, 'consulta.html', {'consulta': consulta, 'dado_medico': dado_medico, 'is_medico': is_medico(request.user)})
+
